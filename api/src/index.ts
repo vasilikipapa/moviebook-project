1fb12066ec22c7b87798b64ffab457e5fb2deb11
Prodromos Pantos <prpanto@gmail.com>
2026-06-08 01:16:40 +0300

    Fix and add await to connect database.

--- api/src/index.ts
+++ api/src/index.ts
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 const database = await useDatabase()
 
-database.connect().catch((error) => {
+await database.connect().catch((error) => {
   console.error('MongoDB connection error:', error)
 })
 
